83337608458b738afbe3e7de9195e38f54019f6e
MZiegenhorn <40473398+MZiegenhorn@users.noreply.github.com>
2021-07-13 09:19:05 -0700

    AVISO code
    habMod... - code to extract FSLE data from AVISO using an FTP
    extract...- use after habMod- allows user to extract relevant times/lats/lons from global FSLE.nc files. Stores output as .mat files with lats, lons, FSLE info

---
+++ extract_FSLE_from_NCfiles.m
@@ -0,0 +1,57 @@
+%created by MAZ on 7/12/2021 to prune downloaded aviso fsle files to
+%relevant times, lats, lons
+clear all
+close all
+clc
+
+infold = 'E:\ftp_test\2007'; %input folder of .nc fsle files downloaded from aviso using habMod_FSLE_from_AVISO.m
+outfold = 'E:\ftp_test\2007\trunc';
+latRange = [10,40]; %range of latitudes for truncated data
+lonRange = [180,250]; %range of longitudes for truncated data
+timeRange = [datenum(2007,1,15,0,0,0),datenum(2007,10,1,0,0,0)]; %what date range do you care about
+inFSLE = dir(fullfile(infold,'*.nc'));
+
+if ~isdir(outfold)
+    mkdir(outfold)
+end
+
+%run through each file and truncate down, save modified version
+%for each file
+for ifile = 1:size(inFSLE,1)
+    curfile = fullfile(inFSLE(ifile).folder,inFSLE(ifile).name);
+    %figure out what day it's from- if day is outside of date range, skip file
+    %and move on
+    time = ncread(curfile,'time');
+    dntime = double(datenum(1950,1,1,0,0,0) + time); %convert time from days since 1950-01-01 00:00:00 to normal MATLAB datenum
+    
+    if dntime >= min(timeRange) & dntime<=max(timeRange)
+        %grab other relevant data to modify
+        lats_temp = ncread(curfile,'lat');
+        lons_temp = ncread(curfile,'lon');
+        theta_max_temp = ncread(curfile,'theta_max');
+        lon_bnds_temp = ncread(curfile,'lon_bnds');
+        lat_bnds_temp = ncread(curfile,'lat_bnds');
+        fsle_max_temp = ncread(curfile,'fsle_max');
+        %also get variables without adjustments, so we can save them in output
+        %file. Not res-saving global attributes; if those are needed should be
+        %extracted from original nc files using ncdisp()
+        crs = ncread(curfile,'crs');
+        
+        trunclats = find(lats_temp>=latRange(1) & lats_temp<=latRange(2));
+        trunclons = find(lons_temp>=lonRange(1) & lons_temp<=lonRange(2));
+        %truncate things down to match
+        lats = double(lats_temp(trunclats));
+        lat_bnds = lat_bnds_temp(trunclats);
+        lons = double(lons_temp(trunclons));
+        lon_bnds = lon_bnds_temp(trunclons);
+        theta_max = theta_max_temp(trunclons,trunclats);
+        fsle_max = fsle_max_temp(trunclons,trunclats);
+        
+        savename = strrep(inFSLE(ifile).name,'.nc','_mod.mat');
+        save(fullfile(outfold,savename),'fsle_max','theta_max','lats','lons',...
+            'lat_bnds','lon_bnds','crs','-v7.3')
+        disp(['Done with file ',curfile])
+    else
+        disp(['Skipping ',curfile,'. Outside of desired time range'])
+    end
+end
---
+++ habMod_FLSE_from_AVISO.m
@@ -0,0 +1,42 @@
+%created by MAZ on 7/12/2021 in order to download fsle data from MATLAB
+%using ftp server
+clear all
+close all
+clc
+
+ftppath = 'ftp-access.aviso.altimetry.fr';%base path in URL from ftp from AVISO
+ftpfolder = 'value-added/lyapunov/delayed-time/global'; %path to specific folder within AVISO ftp that has FSLE data
+outfolder = 'H:\AVISO_FLSEdata'; %will change current directory to this folder so that downloaded files are saved here
+years = {'2005','2006','2007','2008','2009','2010','2011','2012','2013',...
+    '2014','2015','2016','2017','2018','2019','2020'};
+
+username = 'mziegenh@ucsd.edu'; %username and password below used to authenticate connection to ftp
+pw = '28n1Kt';
+
+%use mget to download files into your folder
+for iy = 1:size(years,2)
+    
+%get ftp object
+ftob = ftp(ftppath,username,pw);
+
+    %get your output folder set up
+    outfolderfull = fullfile(outfolder,years{iy});
+    %if output folder doesn't exist, make it
+    if ~isdir(outfolderfull)
+        mkdir(outfolderfull)
+    end
+    
+    %make your output folder the current folder so things save correctly there
+    cd(outfolderfull)
+    disp(['Saving extracted files to ',outfolderfull])
+    
+    %add the year to your filepath
+    ftpfolderfull = [ftpfolder,'/',years{iy},'/'];
+    %set current directory for ftp to point to specific folder
+    cd(ftob,ftpfolderfull);
+    %extract files for that year into the correct folder
+    mget(ftob,'*.nc');
+end
+
+disp(['Done extracting files from ',ftppath,ftpfolder])
+
