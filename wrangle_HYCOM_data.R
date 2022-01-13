2fb52f21b43b9f1a7c578c74aec531c4ce333c51
Rebecca Cohen <30301737+rcohen85@users.noreply.github.com>
2022-01-13 10:53:50 -0800

    Reorganizing script

--- wrangle_HYCOM_data.R
+++ wrangle_HYCOM_data.R
@@ -8,6 +8,19 @@ var = "salinity"
 ######## Action -----------------
 library(ncdf4)
 library(stringr)
+HARPs = t(data.frame(c(41.06165, -66.35155), # WAT_HZ
+                     c(40.22999, -67.97798),  # WAT_OC
+                     c(39.83295, -69.98194),  # WAT_NC
+                     c(39.19192, -72.22735),  # WAT_BC
+                     c(38.37337, -73.36985),  # WAT_WC
+                     c(37.16452, -74.46585),  # NFC
+                     c(35.30183, -74.87895),  # HAT
+                     c(33.66992, -75.9977),   # WAT_GS
+                     c(32.10527, -77.09067),  # WAT_BP
+                     c(30.58295, -77.39002),  # WAT_BS
+                     c(30.27818, -80.22085)))  # JAX_D
+rownames(HARPs) = c("HZ","OC","NC","BC","WC","NFC","HAT","GS","BP","BS","JAX")
+colnames(HARPs) = c("Lat","Lon")
 
 # list all files in given directory
 # find file names containing given covar
@@ -27,21 +40,7 @@ print(ncin)
 temp = ncvar_get(ncin,"water_temp")
 lat = ncvar_get(ncin,"lat")
 lon = ncvar_get(ncin,"lon")
-time = ncvar_get(ncin,"time")
-
-HARPs = t(data.frame(c(41.06165, -66.35155), # WAT_HZ
-         c(40.22999, -67.97798),  # WAT_OC
-         c(39.83295, -69.98194),  # WAT_NC
-         c(39.19192, -72.22735),  # WAT_BC
-         c(38.37337, -73.36985),  # WAT_WC
-         c(37.16452, -74.46585),  # NFC
-         c(35.30183, -74.87895),  # HAT
-         c(33.66992, -75.9977),   # WAT_GS
-         c(32.10527, -77.09067),  # WAT_BP
-         c(30.58295, -77.39002),  # WAT_BS
-         c(30.27818, -80.22085)))  # JAX_D
-rownames(HARPs) = c("HZ","OC","NC","BC","WC","NFC","HAT","GS","BP","BS","JAX")
-colnames(HARPs) = c("Lat","Lon")
+time = ncvar_get(ncin,"time") # might want to name this something generic, since the covar listed in line 5 will change
 
 
 for (i in 1:length(HARPs)){
