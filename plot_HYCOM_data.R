f831c81ed0c94db800518ce5e74fa967d1484214
aadornato <81193849+laurenbaggett@users.noreply.github.com>
2022-01-20 17:05:35 -0800

    Trying to wrangle data into format for easy plotting

--- plot_HYCOM_data.R
+++ plot_HYCOM_data.R
@@ -5,6 +5,19 @@ library(gridExtra)
 library(tidyverse)
 
 #Create our Data frames -----------------------------------------------------------------
+HARPs = t(data.frame(c(41.06165, -66.35155), # WAT_HZ
+                     c(40.22999, -67.97798),  # WAT_OC
+                     c(39.83295, -69.98194),  # WAT_NC
+                     c(39.19192, -72.22735),  # WAT_BC
+                     c(38.37337, -73.36985),  # WAT_WC
+                     c(37.16452, -74.46585),  # NFC
+                     c(35.30183, -74.87895),  # HAT
+                     c(33.66992, -75.9977),   # WAT_GS
+                     c(32.10527, -77.09067),  # WAT_BP
+                     c(30.58295, -77.39002),  # WAT_BS
+                     c(30.27818, -80.22085)))  # JAX_D
+rownames(HARPs) = c("HZ","OC","NC","BC","WC","NFC","HAT","GS","BP","BS","JAX")
+colnames(HARPs) = c("Lat","Lon")
 
 
 #Temperature
@@ -32,12 +45,27 @@ temp_list <- list(temp0m, temp50m, temp100m,
                   temp200m, temp500m, temp1000m,
                   temp3000m, temp4000m)
 
+
 #merge all data frames together
 tempAllDepths <- temp_list %>% reduce(full_join)
 tempAllDepths <- as.data.frame(tempAllDepths)
 
+# loop through HARP sites
+for (i in length(HARPs)){
+  
+  # for each site, find observations (tempAllDepths$Coords) which have Coord values matching that site (HARPs[i])
+  # want to get a bunch of indices
+  # Use aggregate() to organize observations by depth
+  
+  # use those indices pull those obs (tempAllDepths$Covar[indices]) to a new DF with depth given by row, and time stamp by column
+  
+}
+
+
+
+
 #unlist data frame
-tempAllDepths <- as.data.frame(lapply(tempAllDepths, unlist))
+# tempAllDepths <- as.data.frame(lapply(tempAllDepths, unlist))
 
 
 #CREATE CONTOUR PLOTS ---------------------------------------------------------------
