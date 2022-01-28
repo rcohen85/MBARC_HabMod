246831286ef4259ae0b2e02affb8368fcdc8405a
aadornato <81193849+laurenbaggett@users.noreply.github.com>
2022-01-28 12:32:04 -0800

    Bug fixes: take time stamp from file name (no time variable in .nc files); adjust longitude range

--- extract_FSLE_from_NCfiles.R
+++ extract_FSLE_from_NCfiles.R
@@ -1,15 +1,18 @@
 #created by MAZ on 7/26/2021 to extract specific lats, lons, and times from 
 #downloaded AVISO FSLE data .nc files
-rm()
 
+library(stringr)
 
-infolder = 'H:/AVISO_FLSEdata/2007/'
-outfolder = file.path('E:','fsle','2007','trunc') #kind of a nuisance to write it this way, but different types of computers use different file separators, so in the long run it might be good?
-latrange = c(10,40)
-lonrange = c(180,250)
-timerange = c(as.Date('2007/07/1'),as.Date('2007/10/1'))
+infolder = 'F:/AVISO_FLSEdata/2019/'
+outfolder = file.path('E:','fsle','trunc') #kind of a nuisance to write it this way, but different types of computers use different file separators, so in the long run it might be good?
+latrange = c(24,46)
+lonrange = c(-63,-82)
+timerange = c(as.Date('2019/01/1'),as.Date('2019/04/30'))
+
+####--------------------------------------------
 
 allfiles = list.files(infolder,pattern = "*.nc",full.names = TRUE)
+lonrange = 360+lonrange
 
 if (!dir.exists(outfolder)){
   dir.create(outfolder,recursive = TRUE)
@@ -21,8 +24,17 @@ for (i in seq_along(allfiles)){
   ncdata = nc_open(allfiles[i])
   
   #get times
-  time_temp = ncvar_get(ncdata,"time")
-  times = as.Date('1950/1/1') + time_temp
+  # time_temp = ncvar_get(ncdata,"time")
+  
+  # times = as.Date('1950/1/1')
+  
+  # get 6-digit datestamp from file name
+  fileDate = str_extract(allfiles[i],"\\d\\d\\d\\d\\d\\d\\d\\d") 
+  time_temp = paste(str_sub(fileDate,start=1L,end=4L),'-',
+                     str_sub(fileDate,start=5L,end=6L),'-',
+                     str_sub(fileDate,start=7L,end=8L),sep="")
+  
+  times = as.Date(time_temp,format='%Y-%m-%d',tz="UTC")
   
   if (times>=timerange[1] & times<=timerange[2]){
     #get lats
@@ -31,44 +43,46 @@ for (i in seq_along(allfiles)){
     lons_temp = ncvar_get(ncdata,"lon")
     #get other variables
     theta_max_temp = ncvar_get(ncdata,"theta_max")
-    lon_bnds_temp = ncvar_get(ncdata,'lon_bnds')
-    lat_bnds_temp = ncvar_get(ncdata,'lat_bnds')
+    # lon_bnds_temp = ncvar_get(ncdata,'lon_bnds')
+    # lat_bnds_temp = ncvar_get(ncdata,'lat_bnds')
     fsle_max_temp = ncvar_get(ncdata,'fsle_max')
-    crs = ncvar_get(ncdata,'crs')
+    # crs = ncvar_get(ncdata,'crs')
     
     #truncate lats/lons
     latsidx = lats_temp>=latrange[1] & lats_temp<=latrange[2]
-    lonsidx = lons_temp>=lonrange[1]&lons_temp<=lonrange[2]
+    lonsidx = lons_temp<=lonrange[1]&lons_temp>=lonrange[2]
     
     #truncate others to match
     lats = as.numeric(lats_temp[latsidx])
     lons = as.numeric(lons_temp[lonsidx])
-    lat_bnds = as.numeric(lat_bnds_temp[latsidx])
-    lon_bnds = as.numeric(lon_bnds_temp[lonsidx])
-    theta_max = as.numeric(theta_max_temp[lonsidx,latsidx])
-    fsle_max = as.numeric(fsle_max_temp[lonsidx,latsidx])
+    # lat_bnds = as.numeric(lat_bnds_temp[latsidx])
+    # lon_bnds = as.numeric(lon_bnds_temp[lonsidx])
+    theta_max = as.data.frame(t(theta_max_temp[lonsidx,latsidx]))
+    fsle_max = as.data.frame(t(fsle_max_temp[lonsidx,latsidx]))
     
     #save stuff
     #get just the name of the file
     tempname = read.table(text = allfiles[i],sep = '/')
     usename = tempname[4]
-    savename = file.path(outfolder,sub('.nc','.csv',usename))
+    usename = str_remove(usename,"_\\d\\d\\d\\d\\d\\d\\d\\d.nc")
+    savename = file.path(outfolder,paste(usename,'.Rdata',sep=""))
     
     #set up data as dataframe
-    ncframe = data.frame(lats = lats,
-                         lons = lons,
-                         lat_bnds = lat_bnds,
-                         lon_bnds = lon_bnds,
-                         theta_max = theta_max,
-                         fsle_max = fsle_max,
-                         crs = crs)
+    # ncframe = data.frame(lats = lats,
+    #                      lons = lons,
+    #                      lat_bnds = lat_bnds,
+    #                      lon_bnds = lon_bnds,
+    #                      theta_max = theta_max,
+    #                      fsle_max = fsle_max,
+    #                      crs = crs)
     
     #save data frame as .csv
-    write.csv(ncframe,savename)
+    # write.csv(ncframe,savename)
+    save(lats,lons,theta_max,fsle_max,file=savename)
     
     print(paste('Done with file ',allfiles[i]))
   }
     else {
       #if skipping file, say so 
-      print(paste('skipping',allfiles[i],'. Outside of time bounds.'))
+      print(paste('Skipping file ',allfiles[i],', outside of time bounds.'))
     }}
